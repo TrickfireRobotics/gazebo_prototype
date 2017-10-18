f5deee5668abc21bc87da8ca723001a1755aabfa
Adam Krpan <adam.krpan@outlook.com>
2017-10-18 00:04:41 -0700

    Added the lower and upper arm segments and control

--- exc_control/src/DriverStation.cpp
+++ exc_control/src/DriverStation.cpp
@@ -6,7 +6,9 @@
 
 using namespace std;
 
-float armRot = 0.0f;
+float turntableRot = 0.0f;
+float armLowerRot = 0.0f;
+float armUpperRot = 0.0f;
 
 void handle_drive(ros::Publisher vel_pub) {
   geometry_msgs::Twist twist;
@@ -33,16 +35,49 @@ void handle_drive(ros::Publisher vel_pub) {
 void handle_turntable(ros::Publisher turn_pub) {
   // Two keys, < and >, will rotate the arm by 15 degrees each press
   std_msgs::Float64 turn;
+
   if (sf::Keyboard::isKeyPressed(sf::Keyboard::Period)) { // Rotate right, decrease angle
-    armRot -= M_PI / 6;
+    turntableRot -= M_PI / 6;
   }
   if (sf::Keyboard::isKeyPressed(sf::Keyboard::Comma)) { // Rotate left, increase angle
-    armRot += M_PI / 6;
+    turntableRot += M_PI / 6;
   }
 
-  turn.data = armRot;
+  turn.data = turntableRot;
 
   turn_pub.publish(turn);
+}
+
+void handle_lower_arm(ros::Publisher lower_arm_pub) {
+  // Use U and J to raise and lower respectively
+  std_msgs::Float64 turn;
+
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::U)) { // Rotate up, decrease angle
+    armLowerRot -= M_PI / 6;
+  }
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::J)) { // Rotate down, increase angle
+    armLowerRot += M_PI / 6;
+  }
+
+  turn.data = armLowerRot;
+
+  lower_arm_pub.publish(turn);
+}
+
+void handle_upper_arm(ros::Publisher upper_arm_pub) {
+  // Use I and K to raise and lower respectively
+  std_msgs::Float64 turn;
+
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::I)) { // Rotate up, decrease angle
+    armUpperRot -= M_PI / 6;
+  }
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::K)) { // Rotate down, increase angle
+    armUpperRot += M_PI / 6;
+  }
+
+  turn.data = armUpperRot;
+
+  upper_arm_pub.publish(turn);
 }
 
 int main(int argc, char **argv)
@@ -52,6 +87,8 @@ int main(int argc, char **argv)
 
   ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
   ros::Publisher turn_pub = n.advertise<std_msgs::Float64>("/turntable_position_controller/command", 1);
+  ros::Publisher lower_arm_pub = n.advertise<std_msgs::Float64>("/lower_arm_position_controller/command", 1);
+  ros::Publisher upper_arm_pub = n.advertise<std_msgs::Float64>("/upper_arm_position_controller/command", 1);
 
   sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
 
@@ -69,6 +106,8 @@ int main(int argc, char **argv)
         case sf::Event::KeyReleased: {
           handle_drive(vel_pub);
           handle_turntable(turn_pub);
+          handle_lower_arm(lower_arm_pub);
+          handle_upper_arm(upper_arm_pub);
         }
         default: break;
       }
