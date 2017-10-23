00dd67810f1c9f93fc0ca05caf26d10290c5dfe3
Adam Krpan <adam.krpan@outlook.com>
2017-10-23 10:04:31 -0700

    Added the MoveIt package and basic implementation

--- exc_control/src/PointTargeter.cpp
+++ exc_control/src/PointTargeter.cpp
@@ -10,6 +10,8 @@
 #include "tf2_ros/buffer.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include <math.h>
+#include "moveit/robot_model_loader/robot_model_loader.h"
+#include "moveit/robot_state/robot_state.h"
 
 using namespace std;
 
@@ -54,6 +56,22 @@ int main(int argc, char **argv)
   lower_arm_pub = n.advertise<std_msgs::Float64>("exc/lower_arm_position_controller/command", 1);
   upper_arm_pub = n.advertise<std_msgs::Float64>("exc/upper_arm_position_controller/command", 1);
   ros::Subscriber sub = n.subscribe("/clicked_point", 1, pointCallback);
+
+  // Test MoveIt setup
+  robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
+  robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
+  ROS_INFO("Model frame: %s", kinematic_model->getModelFrame().c_str());
+
+  robot_state::RobotStatePtr kinematic_state(new robot_state::RobotState(kinematic_model));
+  kinematic_state->setToDefaultValues();
+  const robot_state::JointModelGroup *joint_model_group = kinematic_model->getJointModelGroup("god");
+  const std::vector<std::string> &joint_names = joint_model_group->getVariableNames();
+  std::vector<double> joint_values;
+  kinematic_state->copyJointGroupPositions(joint_model_group, joint_values);
+  for (std::size_t i = 0; i < joint_names.size(); ++i)
+  {
+    ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
+  }
 
   ros::spin();
 
